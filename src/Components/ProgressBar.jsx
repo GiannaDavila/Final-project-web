8b11d059d7c70340b7942b5f461116ba635ae7a2
GiannaDavila <giannagiannadavila@gmail.com>
2022-12-13 11:57:30 -0500

    final color changes

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -5,18 +5,18 @@ import { Progress, Space } from 'antd';
 const ProgressBar = ({totalProtein, totalCarbs, totalFats, totalCalories}) => (
     <Space wrap className="progressBar" >
       <div className="progressContainer">
-      <Progress type="circle" percent={(100 * totalProtein) / 88} format={()=> totalProtein+"g"}
+      <Progress id="hello" type="circle" percent={(100 * totalProtein) / 88} format={()=> totalProtein+"g"}
        strokeColor={{
-        '0%': '#FC4445',
-        '100%': '#3500D3',
+        '0%': 'orange',
+        '100%': 'red',
       }} />
       <h1>Protein</h1>
       </div>
       <div className="progressContainer">
       <Progress type="circle" percent={(100 * totalCarbs/ 192)} format={()=> totalCarbs+"g"} 
        strokeColor={{
-        '0%': '#FC4445',
-        '100%': '#3500D3',
+        '0%': 'orange',
+        '100%': 'red',
       }}/>
       <h1>Carbs</h1>
       </div>
@@ -24,16 +24,16 @@ const ProgressBar = ({totalProtein, totalCarbs, totalFats, totalCalories}) => (
       <Progress 
       className="progressG" type="circle" percent={ (100 * totalFats/ 38)} format={()=> totalFats+"g"} 
        strokeColor={{
-        '0%': '#FC4445',
-        '100%': '#3500D3',
+        '0%': 'orange',
+        '100%': 'red',
       }}/>
       <h1>Fats</h1>
       </div>
       <div className="progressContainer">
       <Progress type="circle" percent={(100 * totalCalories/ 1440)} format={()=> totalCalories+"cals"} 
        strokeColor={{
-        '0%': '#FC4445',
-        '100%': '#3500D3',
+        '0%': 'orange',
+        '100%': 'red',
       }}/>
         <h1>Calories</h1>
         </div>
